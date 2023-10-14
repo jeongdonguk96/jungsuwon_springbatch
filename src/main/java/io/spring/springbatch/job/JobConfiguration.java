23349bfe470414652f799295ee38ca0e4b5264f2
jungdonguk <foodsmith96@naver.com>
2023-10-14 18:40:24 +0900

    0.7v

--- src/main/java/io/spring/springbatch/job/JobConfiguration.java
+++ src/main/java/io/spring/springbatch/job/JobConfiguration.java
@@ -1,16 +1,20 @@
 package io.spring.springbatch.job;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.job.flow.FlowExecutionStatus;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Configuration
 @RequiredArgsConstructor
@@ -19,37 +23,28 @@ public class JobConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-
     @Bean
     public Job batchjob() {
         return jobBuilderFactory.get("batchjob")
-                .start(step1()) // stp1을 실행한다.
-                .on("FAILED") // step1의 실행결과가 실패면
-                    .to(step2()) // step2를 실행한다.
-                    .on("*") // step2의 실행결과과 상관없이
-                        .stop() // 멈춘다.
-                .from(step1()) // step1의
-                .on("*") // 실행결과가 실패를 제외한 나머지라면
-                    .to(step3()) // step3을 실행한다.
-                    .next(step4()) // step4를 실행한다.
-                    .on("FAILED") // step4의 실행결과가 실패면
-                        .stop() // 멈춘다.
-                .end() // FlowBuilder를 종료한다.
+                .incrementer(new RunIdIncrementer())
+                .start(flow())
+                .end()
                 .build();
-//                .start(step1()) // step1을 실행한다.
-//                .on("COMPLETED") // 실행결과가 성공이면
-//                .to(step3()) // step3을 실행한다.
-//                .from(step1())// step1의
-//                .on("FAILED")// 실행결과가 실패면
-//                .to(step2()) // step2을 실행한다.
-//                .end() // FlowBuilder를 종료한다.
-//                .build();
     }
 
+    @Bean
+    public Flow flow() {
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
+        flowBuilder.start(step1())
+                .next(step2())
+                .end();
+
+        return flowBuilder.build();
+    }
 
     @Bean
     public Step step1() {
-        return stepBuilderFactory.get("step1")
+        return stepBuilderFactory.get("step11")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
@@ -65,7 +60,7 @@ public class JobConfiguration {
 
     @Bean
     public Step step2() {
-        return stepBuilderFactory.get("step2")
+        return stepBuilderFactory.get("step22")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
@@ -81,7 +76,7 @@ public class JobConfiguration {
 
     @Bean
     public Step step3() {
-        return stepBuilderFactory.get("step3")
+        return stepBuilderFactory.get("step33")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
@@ -97,7 +92,7 @@ public class JobConfiguration {
 
     @Bean
     public Step step4() {
-        return stepBuilderFactory.get("step4")
+        return stepBuilderFactory.get("step44")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
