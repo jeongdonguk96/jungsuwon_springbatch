7670092faf43700298056cf530db80ee0bf9ff26
JeongDongUk <foodsmith96@naver.com>
2023-10-16 18:29:29 +0900

    0.8v

--- src/main/java/io/spring/springbatch/job/JobConfiguration.java
+++ src/main/java/io/spring/springbatch/job/JobConfiguration.java
@@ -5,15 +5,18 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -25,40 +28,29 @@ public class JobConfiguration {
     @Bean
     public Job batchjob() {
         return jobBuilderFactory.get("batchjob")
-                .start(step5("step 55555"))
+                .start(step1())
                 .next(step2())
                 .build();
     }
 
     @Bean
-    @JobScope
-    public Step step5(@Value("#{jobParameters['message']}") String message) {
-        System.out.println("message = " + message);
-
-        return stepBuilderFactory.get("step55")
-                .tasklet(tasklet("job1"))
-                .build();
-    }
-
-    @Bean
-    @StepScope
-    public Tasklet tasklet(@Value("#{jobExecutionContext['name']}") String name) {
-        return (contribution, chunkContext) -> {
-            System.out.println(name);
-            return RepeatStatus.FINISHED;
-        };
-    }
-
-    @Bean
     public Step step1() {
-        return stepBuilderFactory.get("step11")
-                .tasklet(new Tasklet() {
+        return stepBuilderFactory.get("step1")
+                .<String, String>chunk(5)
+                .reader(new ListItemReader<>(Arrays.asList("item1, item2, item3, item4, item5")))
+                .processor(new ItemProcessor<String, String>() {
                     @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("=======================");
-                        System.out.println(" >> Hello Spring Batch 11111");
-                        System.out.println("=======================");
-                        return RepeatStatus.FINISHED;
+                    public String process(String item) throws Exception {
+                        Thread.sleep(300);
+                        System.out.println("reader: item = " + item);
+                        return "my " + item;
+                    }
+                })
+                .writer(new ItemWriter<String>() {
+                    @Override
+                    public void write(List<? extends String> items) throws Exception {
+                        Thread.sleep(300);
+                        System.out.println("writer: items = " + items);
                     }
                 })
                 .allowStartIfComplete(true)
@@ -67,50 +59,15 @@ public class JobConfiguration {
 
     @Bean
     public Step step2() {
-        return stepBuilderFactory.get("step22")
+        return stepBuilderFactory.get("step2")
                 .tasklet(new Tasklet() {
                     @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("=======================");
-                        System.out.println(" >> Hello Spring Batch 22222");
-                        System.out.println("=======================");
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step2 done");
                         return RepeatStatus.FINISHED;
                     }
                 })
                 .allowStartIfComplete(true)
                 .build();
     }
-
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step33")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("=======================");
-                        System.out.println(" >> Hello Spring Batch 33333");
-                        System.out.println("=======================");
-                        return RepeatStatus.FINISHED;
-                    }
-                })
-                .allowStartIfComplete(true)
-                .build();
-    }
-
-    @Bean
-    public Step step4() {
-        return stepBuilderFactory.get("step44")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("=======================");
-                        System.out.println(" >> Hello Spring Batch 44444");
-                        System.out.println("=======================");
-                        return RepeatStatus.FINISHED;
-                    }
-                })
-                .allowStartIfComplete(true)
-                .build();
-    }
-
 }
